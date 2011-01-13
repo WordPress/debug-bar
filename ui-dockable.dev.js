e61f1d202252a0565a450f47538544a2831b85e7
koopersmith <koopersmith@git.wordpress.org>
2011-01-13 22:56:01 +0000

    Add ui-dockable.js files.
    git-svn-id: https://plugins.svn.wordpress.org/debug-bar/trunk@332248 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ ui-dockable.dev.js
@@ -0,0 +1,52 @@
+/**
+ * Dockable.
+ **/
+(function($){
+	$.widget("db.dockable", $.ui.mouse, {
+		options: {
+			handle: false,
+			axis: 'y',
+			resize: function() {},
+			resized: function() {}
+		},
+		_create: function() {
+			if ( this.options.axis == 'x' ) {
+				this.page = 'pageX';
+				this.dimension = 'width';
+			} else {
+				this.page = 'pageY';
+				this.dimension = 'height';
+			}
+
+			if ( ! this.options.handle )
+				return;
+
+			this.handle = $( this.options.handle );
+
+			this._mouseInit();
+		},
+		widget: function() {
+			return {
+				element: this.element,
+				handle: this.handle,
+				axis: this.options.axis
+			};
+		},
+		_mouseStart: function(event) {
+			this.d0 = this.element[this.dimension]() + event[this.page];
+		},
+		_mouseDrag: function(event) {
+			var resize = this._trigger( "resize", event, this.widget() );
+
+			// If the resize event returns false, we don't resize.
+			if ( resize === false )
+				return;
+
+			this.element[this.dimension]( this.d0 - event[this.page] );
+			this._trigger( "resized", event, this.widget() );
+		},
+		_mouseCapture: function(event) {
+			return !this.options.disabled && event.target == this.handle[0];
+		}
+	});
+})(jQuery);
