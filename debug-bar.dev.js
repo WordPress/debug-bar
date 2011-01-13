ccd9ff7e9bc684054778bb17492d43272ba5b71a
koopersmith <koopersmith@git.wordpress.org>
2011-01-13 22:54:20 +0000

    New styling, dock the debug bar to the bottom of the window. Make the dock resizable.
    git-svn-id: https://plugins.svn.wordpress.org/debug-bar/trunk@332246 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- debug-bar.dev.js
+++ debug-bar.dev.js
@@ -1,33 +1,99 @@
 (function($) {
 
-$(document).ready( function(){
-	var adminBarLink = $('#wp-admin-bar-debug-bar'),
-		queryList = $('#querylist'),
-		debugMenuLinks = $('.debug-menu-link'),
-		debugMenuTargets = $('.debug-menu-target');
+var debugBar, bounds, wpDebugBar, $win, $body;
+
+bounds = {
+	adminBarHeight: 0,
+	minHeight: 0,
+	marginBottom: 0,
 	
-	adminBarLink.click( function(e){
-		queryList.toggle();
-		e.preventDefault();
-	});
+	inUpper: function(){
+		return debugBar.offset().top - $win.scrollTop() >= bounds.adminBarHeight;
+	},
+	inLower: function(){
+		return debugBar.outerHeight() >= bounds.minHeight
+			&& $win.height() >= bounds.minHeight;
+	},
+	fix: function() {
+		if ( ! bounds.inUpper() )
+			debugBar.height( $win.height() - bounds.adminBarHeight );
+		if ( ! bounds.inLower() )
+			debugBar.height( bounds.minHeight );
+		$body.css( 'margin-bottom', debugBar.height() + bounds.marginBottom );
+	}
+};
+
+wpDebugBar = {
+	init: function(){
+		// Initialize variables.
+		debugBar = $('#querylist');
+		$win = $(window);
+		$body = $(document.body);
+		
+		bounds.minHeight = $('#debug-bar-handle').outerHeight() + $('#debug-bar-menu').outerHeight();
+		bounds.adminBarHeight = $('#wpadminbar').outerHeight();
+		bounds.marginBottom = parseInt( $body.css('margin-bottom'), 10 );
+		
+		wpDebugBar.dock();
+		wpDebugBar.toggle();
+		wpDebugBar.tabs();
+	},
 	
-	debugMenuLinks.click( function(e){
-		var t = $(this);
-		
-		e.preventDefault();
-		
-		if ( t.hasClass('current') )
-			return;
-		
-		// Deselect other tabs and hide other panels.
-		debugMenuTargets.hide();
-		debugMenuLinks.removeClass('current');
-		
-		// Select the current tab and show the current panel.
-		t.addClass('current');
-		// The hashed component of the href is the id that we want to display.
-		$('#' + this.href.substr( this.href.indexOf( '#' ) + 1 ) ).show();
-	});
-});
+	dock: function(){
+		debugBar.dockable({
+			handle: '#debug-bar-handle',
+			resize: function( e, ui ) {
+				return bounds.inUpper() && bounds.inLower();
+			},
+			resized: function( e, ui ) {
+				bounds.fix();
+			}
+		});
+
+		// If the window is resized, make sure the debug bar isn't too large.
+		$win.resize( function(){
+			if ( debugBar.is(':visible') )
+				bounds.fix();
+		});
+	},
+	
+	toggle: function(){
+		$('#wp-admin-bar-debug-bar').click( function(e){
+			e.preventDefault();
+
+			if ( debugBar.is(':hidden') ) {
+				debugBar.show();
+				bounds.fix();
+			} else {
+				debugBar.hide();
+			}
+		});
+	},
+	
+	tabs: function(){
+		var debugMenuLinks = $('.debug-menu-link'),
+			debugMenuTargets = $('.debug-menu-target');
+			
+		debugMenuLinks.click( function(e){
+			var t = $(this);
+
+			e.preventDefault();
+
+			if ( t.hasClass('current') )
+				return;
+
+			// Deselect other tabs and hide other panels.
+			debugMenuTargets.hide();
+			debugMenuLinks.removeClass('current');
+
+			// Select the current tab and show the current panel.
+			t.addClass('current');
+			// The hashed component of the href is the id that we want to display.
+			$('#' + this.href.substr( this.href.indexOf( '#' ) + 1 ) ).show();
+		});
+	}
+};
+
+$(document).ready( wpDebugBar.init );
 
 })(jQuery);
--- debug-bar.php
+++ debug-bar.php
@@ -39,8 +39,9 @@ function debug_bar_menu_init() {
 	return;
 
 	$suffix = defined('SCRIPT_DEBUG') && SCRIPT_DEBUG ? '.dev' : '';
-	wp_enqueue_style( 'admin-bar-debug', plugins_url("debug-bar/debug-bar$suffix.css"), array(), '20110112a' );
-	wp_enqueue_script( 'admin-bar-debug', plugins_url("debug-bar/debug-bar$suffix.js"), array('jquery'), '20110112a' );
+	wp_enqueue_style( 'admin-bar-debug', plugins_url("debug-bar/debug-bar$suffix.css"), array(), '20110113' );
+	wp_enqueue_script( 'admin-bar-ui-dockable', plugins_url("debug-bar/ui-dockable$suffix.js"), array('jquery-ui-mouse'), '20110113' );
+	wp_enqueue_script( 'admin-bar-debug', plugins_url("debug-bar/debug-bar$suffix.js"), array('jquery', 'admin-bar-ui-dockable'), '20110113' );
 
 	// Silence E_NOTICE for deprecated usage.
 	foreach ( array( 'function', 'file', 'argument' ) as $item )
@@ -79,14 +80,15 @@ function debug_bar_list() {
 		return;
 
 	?>
-<div align='left' id='querylist'>
+<div id='querylist'>
 
-<h1><?php printf( __('Debugging blog #%d on %s'), $GLOBALS['blog_id'], php_uname( 'n' ) ); ?></h1>
-<div id="debug-status">
-<p class="left"></p>
-<p class="right"><?php printf( __('PHP Version: %1$s, DB Version: %2$s'), phpversion(), $wpdb->db_version() ); ?></p>
-</div>
-<ul id="debug-menu-links">
+<div id='debug-bar-handle'></div>
+<div id='debug-bar-menu'>
+	<div id="debug-status">
+		<span id="debug-status-title"><?php printf( __('Blog #%d on %s'), $GLOBALS['blog_id'], php_uname( 'n' ) ); ?></span>
+		| <span id="debug-status-version"><?php printf( __('PHP: %1$s | DB: %2$s'), phpversion(), $wpdb->db_version() ); ?></span>
+	</div>
+	<ul id="debug-menu-links">
 
 <?php
 	$current = ' current';
@@ -101,7 +103,8 @@ function debug_bar_list() {
 		$current = '';
 	endforeach; ?>
 
-</ul>
+	</ul>
+</div>
 
 <div id="debug-menu-targets"><?php	$current = ' style="display: block"'; foreach ( $debugs as $debug => $debug_output ) : ?>
 
