0ec79b5bcc771587a9486f38ea4db468746661c6
Peter Westwood <westi@git.wordpress.org>
2011-12-17 16:02:37 +0000

    Debug Bar: Backtrace info for PHP Notices and Warnings and for Deprecated calls.§
    git-svn-id: https://plugins.svn.wordpress.org/debug-bar/trunk@476807 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- panels/class-debug-bar-php.php
+++ panels/class-debug-bar-php.php
@@ -27,27 +27,29 @@ class Debug_Bar_PHP extends Debug_Bar_Panel {
 	}
 
 	function error_handler( $type, $message, $file, $line ) {
-		$_key = md5( $file . ':' . $line . ':' . $message );
+		if ( error_reporting() ) {
+			$_key = md5( $file . ':' . $line . ':' . $message );
 
-		switch ( $type ) {
-			case E_WARNING :
-			case E_USER_WARNING :
-				$this->warnings[$_key] = array( $file.':'.$line, $message );
-				break;
-			case E_NOTICE :
-			case E_USER_NOTICE :
-				$this->notices[$_key] = array( $file.':'.$line, $message );
-				break;
-			case E_STRICT :
-				// TODO
-				break;
-			case E_DEPRECATED :
-			case E_USER_DEPRECATED :
-				// TODO
-				break;
-			case 0 :
-				// TODO
-				break;
+			switch ( $type ) {
+				case E_WARNING :
+				case E_USER_WARNING :
+					$this->warnings[$_key] = array( $file.':'.$line, $message, wp_debug_backtrace_summary( __CLASS__ ) );
+					break;
+				case E_NOTICE :
+				case E_USER_NOTICE :
+					$this->notices[$_key] = array( $file.':'.$line, $message, wp_debug_backtrace_summary( __CLASS__ ) );
+					break;
+				case E_STRICT :
+					// TODO
+					break;
+				case E_DEPRECATED :
+				case E_USER_DEPRECATED :
+					// TODO
+					break;
+				case 0 :
+					// TODO
+					break;
+			}
 		}
 
 		if ( null != $this->real_error_handler )
@@ -62,17 +64,25 @@ class Debug_Bar_PHP extends Debug_Bar_Panel {
 		echo '<h2><span>Total Notices:</span>' . number_format( count( $this->notices ) ) . "</h2>\n";
 		if ( count( $this->warnings ) ) {
 			echo '<ol class="debug-bar-php-list">';
-			foreach ( $this->warnings as $location_message) {
-				list( $location, $message) = $location_message;
-				echo "<li class='debug-bar-php-warning'>WARNING: ".str_replace(ABSPATH, '', $location) . ' - ' . strip_tags($message). "</li>";
+			foreach ( $this->warnings as $location_message_stack ) {
+				list( $location, $message, $stack) = $location_message_stack;
+				echo '<li class="debug-bar-php-warning">WARNING: ';
+				echo str_replace(ABSPATH, '', $location) . ' - ' . strip_tags($message);
+				echo '<br/>';
+				echo $stack;
+				echo '</li>';
 			}
 			echo '</ol>';
 		}
 		if ( count( $this->notices ) ) {
 			echo '<ol class="debug-bar-php-list">';
-			foreach ( $this->notices as $location_message) {
-				list( $location, $message) = $location_message;
-				echo "<li  class='debug-bar-php-notice'>NOTICE: ".str_replace(ABSPATH, '', $location) . ' - ' . strip_tags($message). "</li>";
+			foreach ( $this->notices as $location_message_stack) {
+				list( $location, $message, $stack) = $location_message_stack;
+				echo '<li class="debug-bar-php-notice">NOTICE: ';
+				echo str_replace(ABSPATH, '', $location) . ' - ' . strip_tags($message);
+				echo '<br/>';
+				echo $stack;
+				echo '</li>';
 			}
 			echo '</ol>';
 		}
@@ -80,3 +90,4 @@ class Debug_Bar_PHP extends Debug_Bar_Panel {
 
 	}
 }
+
