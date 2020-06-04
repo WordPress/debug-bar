ba7502a68bba7c72e91595c1d7d268e70d83e622
Dion Hulse <dd32@git.wordpress.org>
2020-06-04 03:31:10 +0000

    Debug Bar: Bump the CSS version, missed in [2307887].
    git-svn-id: https://plugins.svn.wordpress.org/debug-bar/trunk@2317745 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- debug-bar.php
+++ debug-bar.php
@@ -111,7 +111,7 @@ class Debug_Bar {
 	function enqueue() {
 		$suffix = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '.dev' : '';
 
-		wp_enqueue_style( 'debug-bar', plugins_url( "css/debug-bar$suffix.css", __FILE__ ), array(), '20170515' );
+		wp_enqueue_style( 'debug-bar', plugins_url( "css/debug-bar$suffix.css", __FILE__ ), array(), '20200519' );
 
 		wp_enqueue_script( 'debug-bar', plugins_url( "js/debug-bar$suffix.js", __FILE__ ), array( 'jquery' ), '20170515', true );
 
