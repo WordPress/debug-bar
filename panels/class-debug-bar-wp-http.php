1118f56feb3fa0a067cb9d7e35efe45b90f5d960
Andrija Vučinić <aidvu@git.wordpress.org>
2020-11-12 16:56:56 +0000

    debug-bar: Release 1.1.1
    git-svn-id: https://plugins.svn.wordpress.org/debug-bar/tags/1.1.1@2417345 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- panels/class-debug-bar-wp-http.php
+++ panels/class-debug-bar-wp-http.php
@@ -1,48 +1,24 @@
 <?php
 
-class Debug_Bar_WP_Http_Request {
-	public $time_start;
-	public $time_end;
-
-	public $request;
-	public $result;
-	public $stack_trace;
-
-	function __construct( $request ) {
-		$this->time_start = microtime( true );
-		if ( ! isset( $_GET['fullbody'] ) ) {
-			$request['args']['body'] = '[omitted]';
-		}
-		$this->request = $request;
-	}
-
-	function end( $result ) {
-		$this->time_end = microtime( true );
-		if ( is_array( $result['r'] ) && ! isset( $_GET['fullbody'] ) ) {
-			$result['r']['body'] = '[omitted]';
-			$result['args']['body'] = '[omitted]';
-		}
-		$this->result = $result;
-		$this->stack_trace = wp_debug_backtrace_summary( null, 0, false );
-	}
-}
-
 class Debug_Bar_WP_Http extends Debug_Bar_Panel {
-	public $requests = array();
+	public $requests = [];
+
+	public $time_limit = 250; // milliseconds
+	public $total_time = 0;
+	public $num_errors = 0;
 
 	function early_init() {
-		add_filter( 'http_request_args', array( $this, 'before_http_request' ), 10, 3 );
-		add_action( 'http_api_debug', array( $this, 'after_http_request' ), 10, 5 );
+		add_filter( 'http_request_args', [ $this, 'before_http_request' ], 10, 3 );
+		add_filter( 'http_api_debug', [ $this, 'after_http_request' ], 10, 5 );
 	}
 
 	function before_http_request( $args, $url ) {
-		$request = new Debug_Bar_WP_Http_Request( array(
-			'args' => $args,
-			'url' => $url,
-		) );
+		$args['time_start'] = microtime( true );
 
-		$this->requests["$request->time_start"] = $request;
-		$args['time_start'] = $request->time_start;
+		$this->requests["{$args['time_start']}"] = [
+			'url' => $url,
+			'args' => $args
+		];
 
 		return $args;
 	}
@@ -52,17 +28,43 @@ class Debug_Bar_WP_Http extends Debug_Bar_Panel {
 			return;
 		}
 
-		$request =& $this->requests["{$args['time_start']}"];
-		$request->end( array(
-			'r' => $response,
-			'class' => $class,
-			'args' => $args,
-			'url' => $url,
-		) );
+		$args['time_stop'] = microtime( true );
+
+		$args['duration'] = $args['time_stop'] - $args['time_start'];
+		$args['duration'] *= 1000;
+
+		$this->total_time += $args['duration'];
+
+		if ( $this->is_request_error( $response ) ) {
+			$this->num_errors++;
+		} else {
+			if ( ! isset( $_GET['fullbody'] ) ) {
+				$response['body'] = '[omitted]';
+				unset( $response['http_response'] );
+			}
+		}
+
+		$this->requests["{$args['time_start']}"] = array_merge(
+			$this->requests["{$args['time_start']}"],
+			[
+				'r' => $response,
+				'class' => $class,
+				'args' => $args,
+				'url' => $url,
+				'stack_trace' => wp_debug_backtrace_summary( null, 0, false ),
+			]
+		);
 	}
 
-	function count_errors() {
-		return 0;
+	function is_request_error( $response ) {
+		if (
+			is_wp_error( $response )
+			|| $response['response']['code'] >= 400
+		) {
+			return true;
+		}
+
+		return false;
 	}
 
 	function init() {
@@ -74,68 +76,130 @@ class Debug_Bar_WP_Http extends Debug_Bar_Panel {
 	}
 
 	function debug_bar_classes( $classes ) {
-		if ( $this->count_errors() ) {
+		if (
+			$this->num_errors > 0
+			|| $this->total_time > $this->time_limit
+		) {
 			$classes[] = 'debug-bar-php-warning-summary';
 		}
-
 		return $classes;
 	}
 
 	function render() {
-		$out = "
-		<style>
-			#pdbhttp { clear: left; }
-			#pdbhttp .err { background-color: #ffebe8; border: 1px solid #c00; }
-			#pdbhttp th, #pdbhttp td { padding: 8px; }
-			#pdbhttp pre { font-family: monospace; }
-		</style>\n";
-		$out .= "<script>function pdbhttp_toggle(e) {jQuery(e).toggle();}</script>";
-		$out .= "<table id='pdbhttp'>\n";
-		$out .= "<thead><tr><th>More</th><th>Start</th><th>Duration</th><th>Method</th><th>URL</th><th>Code</th></tr></thead>\n<tbody>\n";
-
-		$total_time = 0;
-		$total_errors = 0;
-		foreach ( array_values( $this->requests ) as $i => $r ) {
-			$start = $r->time_start - $_SERVER['REQUEST_TIME_FLOAT'];
-			$elapsed = $r->time_end - $r->time_start;
-			$total_time += $elapsed;
-
-			$class = '';
-			$code = '';
-			if ( is_wp_error( $r->result['r'] ) || $r->result['r']['response']['code'] >= 400 ) {
-				$class = 'err';
-				$total_errors++;
-				if ( is_wp_error( $r->result['r'] ) ) {
-					$code = $r->result['r']->get_error_code();
-				} else {
-					$code = $r->result['r']['response']['code'];
-				}
-			}
-
-			$out .= "<tr class='$class'>";
-			$out .= "<td><a onclick='pdbhttp_toggle(\"#pdbhttp_$i\")'>Toggle</a></td>";
-			$out .= "<td>" . number_format( sprintf( '%0.1f', $start * 1000 ), 1 ) . " ms</td>";
-			$out .= "<td>" . number_format( sprintf( '%0.1f', $elapsed * 1000 ), 1 ) . " ms</td>";
-			$out .= "<td>{$r->request['args']['method']}</td>";
-			$out .= "<td>{$r->request['url']}</td>";
-			$out .= "<td>{$code}</td>";
-			$out .= "</tr>\n";
-			$out .= "<tr id='pdbhttp_$i' style='display: none'><td colspan=5><pre>" . esc_html( print_r( $r, 1 ) ) . "</pre></td></tr>\n";
-		}
-		$out .= "</tbody>\n</table>\n";
-
-		$heading = '';
-		$heading .= '<h2><span>HTTP Requests:</span>' . number_format( count( $this->requests ) ) . "</h2>\n";
-		$heading .= '<h2><span>Total Elapsed:</span>' . number_format( sprintf( '%0.1f', $total_time * 1000 ), 1 ) . " ms</h2>\n";
-		$heading .= '<h2><span>Errors:</span>' . number_format( $total_errors ) . "</h2>\n";
+		$num_requests = number_format( count( $this->requests ) );
+		$elapsed = number_format( $this->total_time, 1 );
+		$num_errors = number_format( $this->num_errors );
 
 		if ( isset( $_GET['fullbody'] ) ) {
-			$heading .= '<p style="clear:left">Request and response bodies are included. <a href="' . esc_attr( remove_query_arg( 'fullbody' ) ) . '">Reload with those omitted.</a>';
+			$fullbody = '<p style="clear:left">Request and response bodies are included. <a href="' . esc_attr( remove_query_arg( 'fullbody' ) ) . '">Reload with those omitted.</a>';
 		} else {
-			$heading .= '<p style="clear:left">Request and response bodies are omitted. <a href="' . esc_attr( add_query_arg( 'fullbody', 'please' ) ) . '">Reload with those included.</a>';
+			$fullbody = '<p style="clear:left">Request and response bodies are omitted. <a href="' . esc_attr( add_query_arg( 'fullbody', 'please' ) ) . '">Reload with those included.</a>';
 		}
 
-		$out = $heading . $out;
+		$css_errors = '';
+		if (
+			$this->num_errors > 0
+			|| $this->total_time > $this->time_limit
+		) {
+			$css_errors = "#wp-admin-bar-debug-bar-Debug_Bar_WP_Http, #debug-menu-link-Debug_Bar_WP_Http { background-color: #d00 !important; background-image: -moz-linear-gradient(bottom,#f44,#d00) !important; background-image: -webkit-gradient(linear,left bottom,left top,from(#f44),to(#d00)) important; }\n";
+		}
+
+		$elapsed_class = '';
+		if ( $this->num_errors > 0 ) {
+			$elapsed_class = 'debug_bar_http_error';
+		}
+
+		$errors_class = '';
+		if ( $this->total_time > $this->time_limit ) {
+			$errors_class = 'debug_bar_http_error';
+		}
+
+		$out =<<<HTML
+<style>
+	#debug_bar_http { clear: left; }
+	#debug_bar_http .err, .debug_bar_http_error { background-color: #ffebe8; border: 1px solid #c00 !important; }
+	#debug_bar_http th, #debug_bar_http td { padding: 8px; }
+	#debug_bar_http pre { font-family: monospace; }
+	{$css_errors}
+</style>
+
+<script>
+function debug_bar_http_toggle( id ) {
+	var e = document.getElementById( id );
+	if ( e.style.display === "" ) {
+		e.style.display = "none";
+	} else {
+		e.style.display = "";
+	}
+}
+</script>
+
+<h2><span>HTTP Requests:</span> {$num_requests}</h2>
+<h2 class="{$elapsed_class}"><span>Total Elapsed:</span> {$elapsed} ms</h2>
+<h2 class="{$errors_class}"><span>Errors:</span> {$num_errors}</h2>
+
+{$fullbody}
+
+<table id="debug_bar_http">
+	<thead>
+		<tr>
+			<th>More</th>
+			<th>Start</th>
+			<th>Duration</th>
+			<th>Method</th>
+			<th>URL</th>
+			<th>Code</th>
+		</tr>
+	</thead>
+	<tbody>
+HTML;
+
+		foreach( $this->requests as $i => $r ) {
+			$class = '';
+			if (
+				$this->is_request_error( $r['r'] )
+				|| $r['args']['duration'] > $this->time_limit
+			) {
+				$class = 'err';
+			}
+
+			$start = $r['args']['time_start'] - $_SERVER['REQUEST_TIME_FLOAT'];
+			$start *= 1000;
+			$start = number_format( $start, 1 );
+
+			$duration = number_format( $r['args']['duration'], 1 );
+			$method = esc_html( $r['args']['method'] );
+			$url = esc_html( $r['url'] );
+
+			if ( is_wp_error( $r['r'] ) ) {
+				$code = esc_html( $r['r']->get_error_code() );
+			} else {
+				$code = esc_html( $r['r']['response']['code'] );
+			}
+
+			$details = esc_html( print_r( $r, true ) );
+
+			$record_id = 'debug_bar_http_record_' . md5( $i );
+			$out .=<<<HTML
+		<tr class="{$class}">
+			<td><a onclick="debug_bar_http_toggle( '{$record_id}' );">Toggle</a></td>
+			<td>{$start} ms</td>
+			<td>{$duration} ms</td>
+			<td>{$method}</td>
+			<td>{$url}</td>
+			<td>{$code}</td>
+		</tr>
+
+		<tr id="{$record_id}" style="display: none">
+			<td colspan="5"><pre>{$details}</pre></td>
+		</tr>
+HTML;
+		}
+
+		$out .=<<<HTML
+	</tbody>
+</table>
+HTML;
 
 		echo $out;
 	}
