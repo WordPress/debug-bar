58c8d18d214de8665ebd4db9438bdc317832f62e
John James Jacoby <johnjamesjacoby@me.com>
2025-12-10 12:59:27 -0600

    Whitespace fixes.
    See: https://github.com/WordPress/debug-bar/pull/16/commits/d2d0f88714345a552c163f7a9f3cd7e82bd6186e
    See: #16

--- js/debug-bar.dev.js
+++ js/debug-bar.dev.js
@@ -23,7 +23,7 @@ var wpDebugBar;
 
 		toggle: {
 			init: function init() {
-				$( '#wp-admin-bar-debug-bar' ).on('click', function onClickAdminBarMenu( event ) {
+				$( '#wp-admin-bar-debug-bar' ).on( 'click', function onClickAdminBarMenu( event ) {
 					event.preventDefault();
 
 					// Click on submenu item.
@@ -66,7 +66,7 @@ var wpDebugBar;
 			var debugMenuLinks = $( '.debug-menu-link' ),
 				debugMenuTargets = $( '.debug-menu-target' );
 
-			debugMenuLinks.on('click', function onClickLink( event ) {
+			debugMenuLinks.on( 'click', function onClickLink( event ) {
 				var $this = $( this );
 
 				event.preventDefault();
@@ -91,7 +91,7 @@ var wpDebugBar;
 				var actions = $( '#debug-bar-actions' );
 
 				// Close the panel with the esc key if it's open.
-				$( document ).on('keydown', function maybeClosePanel( event ) {
+				$( document ).on( 'keydown', function maybeClosePanel( event ) {
 					var key = event.key || event.which || event.keyCode;
 
 					if ( (27 === key || 'Escape' === key) && api.isVisible() ) {
@@ -100,9 +100,9 @@ var wpDebugBar;
 					}
 				} );
 
-				$( '.maximize', actions ).on('click', api.actions.maximize );
-				$( '.restore',  actions ).on('click', api.actions.restore  );
-				$( '.close',    actions ).on('click', api.actions.close    );
+				$( '.maximize', actions ).on( 'click', api.actions.maximize );
+				$( '.restore',  actions ).on( 'click', api.actions.restore  );
+				$( '.close',    actions ).on( 'click', api.actions.close    );
 			},
 			maximize: function maximize() {
 				api.body.removeClass( 'debug-bar-partial' );
